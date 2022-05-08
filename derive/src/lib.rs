c6bba8328d26a7cfb81abae5a1a91e11fafaa29a
TheoOiry <theo.oiry@yahoo.fr>
2022-05-08 02:22:42 +0200

    Make the Validator derive work for enums

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate proc_macro;
 extern crate proc_macro2;
 
+use crate::parsing::from_ast;
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
-use crate::parsing::{Field, fields_from_data};
 
 mod parsing;
 
@@ -18,18 +18,13 @@ pub fn derive_validator(input: proc_macro::TokenStream) -> proc_macro::TokenStre
 fn expand_derive_validator(input: DeriveInput) -> TokenStream {
     let name = input.ident;
     let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
-    let fields_checking: Vec<TokenStream> = fields_from_data(&input.data)
-        .iter()
-        .map(Field::serialize_rules)
-        .collect();
+    let body = from_ast(&input.data).body(&name);
 
     quote! {
         impl #impl_generics type_rules::Validator for #name #type_generics #where_clause {
             fn check_validity(&self) -> Result<(), String> {
-                #( #fields_checking )*
-                Ok(())
+                #body
             }
         }
     }
 }
-
--- derive/src/parsing.rs
+++ derive/src/parsing.rs
@@ -1,32 +1,75 @@
 use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::Token;
-use quote::quote;
 
-pub struct Field<'a> {
+pub enum Data {
+    Struct(Struct),
+    Enum(Vec<Variant>),
+}
+
+pub struct Struct {
+    fields: Vec<Field>,
+    style: Style,
+}
+
+pub struct Field {
     member: syn::Member,
     rules: Vec<syn::Expr>,
-    ty: &'a syn::Type
 }
 
-struct RuleAttributeParser {
-    pub expr: Punctuated<syn::Expr, Token![,]>,
+pub enum Style {
+    Struct,
+    Tuple,
+    Unit,
 }
 
+pub struct Variant {
+    ident: syn::Ident,
+    def: Struct,
+}
 
-pub fn fields_from_data(data: &syn::Data) -> Vec<Field> {
-    match *data {
-        syn::Data::Struct(ref data) => match data.fields {
-            syn::Fields::Named(ref fields) => fields_from_pun(&fields.named),
-            syn::Fields::Unnamed(ref fields) => fields_from_pun(&fields.unnamed),
-            syn::Fields::Unit => Vec::new(),
-        },
-        _ => panic!("Validator derive is only implemented for Struct"),
+struct RuleAttribute {
+    expr: Punctuated<syn::Expr, Token![,]>,
+}
+
+pub fn from_ast(data: &syn::Data) -> Data {
+    match data {
+        syn::Data::Enum(data) => Data::Enum(enum_from_ast(&data.variants)),
+        syn::Data::Struct(data) => Data::Struct(struct_from_ast(&data.fields)),
+        syn::Data::Union(_) => panic!("Validator derive is not implemented for Union"),
     }
 }
 
-fn fields_from_pun(fields: &Punctuated<syn::Field, Token![,]>) -> Vec<Field> {
+fn enum_from_ast(variants: &Punctuated<syn::Variant, Token![,]>) -> Vec<Variant> {
+    variants
+        .iter()
+        .map(|var| Variant {
+            ident: var.ident.clone(),
+            def: struct_from_ast(&var.fields),
+        })
+        .collect()
+}
+
+fn struct_from_ast(fields: &syn::Fields) -> Struct {
+    match fields {
+        syn::Fields::Named(fields) => Struct {
+            fields: fields_from_ast(&fields.named),
+            style: Style::Struct,
+        },
+        syn::Fields::Unnamed(fields) => Struct {
+            fields: fields_from_ast(&fields.unnamed),
+            style: Style::Tuple,
+        },
+        syn::Fields::Unit => Struct {
+            fields: Vec::new(),
+            style: Style::Unit,
+        },
+    }
+}
+
+fn fields_from_ast(fields: &Punctuated<syn::Field, Token![,]>) -> Vec<Field> {
     fields
         .iter()
         .enumerate()
@@ -35,27 +78,12 @@ fn fields_from_pun(fields: &Punctuated<syn::Field, Token![,]>) -> Vec<Field> {
                 Some(ident) => syn::Member::Named(ident.clone()),
                 None => syn::Member::Unnamed(i.into()),
             },
-            rules: RuleAttributeParser::parse_attributes(&field.attrs),
-            ty: &field.ty,
+            rules: RuleAttribute::parse_attributes(&field.attrs),
         })
         .collect()
 }
 
-impl<'a> Field<'a> {
-    pub fn serialize_rules(&self) -> TokenStream {
-        let Self { rules, member, ty } = &self;
-        match ty {
-            syn::Type::Reference(_) => quote! {
-                #( type_rules::rules::Rule::check(&#rules, self.#member)?; )*
-            },
-            _ => quote! {
-                #( type_rules::rules::Rule::check(&#rules, &self.#member)?; )*
-            }
-        }
-    }
-}
-
-impl Parse for RuleAttributeParser {
+impl Parse for RuleAttribute {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         Ok(Self {
             expr: input.parse_terminated(syn::Expr::parse)?,
@@ -63,20 +91,95 @@ impl Parse for RuleAttributeParser {
     }
 }
 
-impl RuleAttributeParser {
+impl RuleAttribute {
     pub fn parse_attributes(attrs: &[syn::Attribute]) -> Vec<syn::Expr> {
-        attrs
-            .iter()
-            .flat_map(Self::parse_attribute)
-            .collect()
+        attrs.iter().flat_map(Self::parse_attribute).collect()
     }
 
     fn parse_attribute(attr: &syn::Attribute) -> Vec<syn::Expr> {
-        attr
-            .parse_args::<Self>()
+        attr.parse_args::<Self>()
             .unwrap()
             .expr
             .into_iter()
             .collect()
+    }
+}
+
+impl Data {
+    pub fn body(&self, ident: &syn::Ident) -> TokenStream {
+        match self {
+            Data::Enum(variants) => Self::enum_body(variants, ident),
+            Data::Struct(data) => Self::struct_body(data),
+        }
+    }
+
+    fn enum_body(variants: &[Variant], ident: &syn::Ident) -> TokenStream {
+        let variants_arms = variants.iter().map(|variant| variant.match_arm(ident));
+        quote! {
+            match self {
+                #( #variants_arms ),*
+            }
+        }
+    }
+
+    fn struct_body(data: &Struct) -> TokenStream {
+        let fields_rules = data.fields.iter().map(Field::rules);
+        quote! {
+            #( #fields_rules )*
+            Ok(())
+        }
+    }
+}
+
+impl Variant {
+    fn match_arm(&self, data_ident: &syn::Ident) -> TokenStream {
+        let case = self.match_arm_case(data_ident);
+        let fields_rules = self.def.fields.iter().map(Field::rules_named);
+        quote! {
+            #case => {
+                #( #fields_rules )*
+                Ok(())
+            }
+        }
+    }
+
+    fn match_arm_case(&self, data_ident: &syn::Ident) -> TokenStream {
+        let ident = &self.ident;
+        let fields_names = self.def.fields.iter().map(Field::get_named_ident);
+        match self.def.style {
+            Style::Struct => quote! {
+                #data_ident::#ident{#( #fields_names ),*}
+            },
+            Style::Tuple => quote! {
+                #data_ident::#ident(#( #fields_names ),*)
+            },
+            Style::Unit => quote! {
+                #data_ident::#ident
+            },
+        }
+    }
+}
+
+impl Field {
+    fn rules(&self) -> TokenStream {
+        let Self { rules, member } = &self;
+        quote! {
+            #( type_rules::Rule::check(&#rules, &self.#member)?; )*
+        }
+    }
+
+    fn rules_named(&self) -> TokenStream {
+        let ident = self.get_named_ident();
+        let rules = &self.rules;
+        quote! {
+            #( type_rules::Rule::check(&#rules, &#ident)?; )*
+        }
+    }
+
+    fn get_named_ident(&self) -> syn::Ident {
+        match &self.member {
+            syn::Member::Named(ident) => ident.clone(),
+            syn::Member::Unnamed(index) => format_ident!("__field{}", index),
+        }
     }
 }
